11de53a87d4e60d2f1179338fabaaab2649f3b5f
Murithi <kelvinmurithi@live.com>
2024-11-28 15:48:31 +0300

    Update LangChainWorkflow class

--- langchain_workflow.py
+++ langchain_workflow.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -8,4 +9,17 @@ class LangChainWorkflow:
         self.api_key = api_key
 
     def execute_workflow(self, user_input):
-        pass
+        # API request
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'user_input': user_input
+        }
+        response = requests.post(f'{self.base_url}/execute_workflow', headers=headers, json=data)
+
+        if response.status_code == 200:
+            return response.json().get('workflow_result', 'Workflow executed successfully.')
+        else:
+            return 'I am sorry, I could not execute the workflow at this time.'
